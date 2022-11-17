b22dafb9c8373b4b18573c8944b28e6fb9954c3a
Raluca59 <erica_raluk@yahoo.com>
2022-11-17 16:26:22 +0000

    Add global variables

---
+++ run.py
@@ -0,0 +1,19 @@
+import random
+import time
+
+#Global variable for grid
+grid = [[]]
+#Global variable for grid size
+grid_size = 10
+#Global variable for ships
+numb_ships = 3
+#global variable for ships sunk 
+ships_sunk = 0
+#Global variable for bullets 
+bullets_left = 60
+#Global variable for ship position
+ship_position = [[]]
+#Global variable for game status
+game_over = False
+#Global variable for alphabet
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
